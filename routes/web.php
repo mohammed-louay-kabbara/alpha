415a8294f499f1192909baacf23d4033bbc7df01
Doha05a <mohamedlouayk@gmail.com>
2025-06-02 19:34:07 +0300

    تعديل واجهة  تحقق من كلمة السر8

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+
+
+Route::get('/test-mail', function () {
+    Mail::raw('اختبار الإرسال عبر SMTP', function ($message) {
+        $message->to('mohamedlouayk@gmail.com')
+                ->subject('رسالة اختبار');
+    });
+    return 'تم الإرسال!';
+});
 
 Route::get('/', function () {
     return view('welcome');

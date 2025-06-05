ede78dd440c68b558f27841502ccd3957b7c76c3
Doha05a <mohamedlouayk@gmail.com>
2025-06-05 12:29:18 +0300

    تعديل واجهة الريلز19

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use  App\Http\Controllers\AuthController;
 use  App\Http\Controllers\ReelsController;
 use  App\Http\Controllers\ReelLikesController;
+use  App\Http\Controllers\ReelCommentsController;
 
 
 

a7449382252ace39ac9ce9071efd859f695357ca
Doha05a <mohamedlouayk@gmail.com>
2025-06-10 23:40:42 +0300

    تعديل واجهة إضافة منتج10

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -25,7 +25,7 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {  
-        dd($request->media);
+       
 
         $request->validate([
             'name' => 'required', 

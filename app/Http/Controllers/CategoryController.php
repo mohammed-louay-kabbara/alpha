49c07d20b80d5400aa38a8c0841693028e13c1fe
Doha05a <mohamedlouayk@gmail.com>
2025-06-10 18:26:08 +0300

    تعديل واجهة إضافة منتج5

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
     public function index()
     {
         $categories = category::get();
-        return response()->json(['categories' => $categories], 200);
+        return response()->json([$categories], 200);
     }
 
     /**
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -22,9 +22,7 @@ class ProductController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {  
         $request->validate([

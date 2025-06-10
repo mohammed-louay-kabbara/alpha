3aaae97d9e00025c78ea4e26a9b55bfe2efc3927
Doha05a <mohamedlouayk@gmail.com>
2025-06-11 00:02:34 +0300

    تعديل واجهة إضافة منتج13

--- app/Models/product.php
+++ app/Models/product.php
@@ -9,6 +9,6 @@ class product extends Model
        protected $fillable = ['user_id', 'name' , 'price','category_id','is_approved','description'];
 
        public function files(){
-         return $this->hasMany(ProductFile::class);
+         return $this->hasMany(product_file::class);
        }
 }

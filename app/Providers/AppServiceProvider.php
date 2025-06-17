6853d4d66132dfa3f1fb8bfa4260ef8b1b197e0a
Doha05a <mohamedlouayk@gmail.com>
2025-06-17 20:24:26 +0300

    تعديل واجهة عرض المنتج26

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
     Relation::morphMap([
-        'reels' => App\Models\reels::class,
-        'product' => App\Models\product::class,
+        'reels' => \App\Models\reels::class,
+        'product' => \App\Models\product::class,
     ]);
     }
 }

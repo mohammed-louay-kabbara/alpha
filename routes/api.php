f719bbb9d7df63dc6f6e52e1b3b196bbae8b5f85
Doha05a <mohamedlouayk@gmail.com>
2025-06-23 23:20:30 +0300

    تعديل واجهة لوحة التحكم16

--- routes/api.php
+++ routes/api.php
@@ -41,7 +41,8 @@ Route::group([
     Route::post('count_profile', [AuthController::class, 'count_profile']);
     Route::post('info_user', [AuthController::class, 'info_user']);
     Route::resource('reels', ReelsController::class);
-    
+    Route::get('show_products/{id}',[ProductController::class,'show']);
+    Route::get('show_reels/{id}',[ReelsController::class,'show']);
     Route::resource('reelLikes', ReelLikesController::class);
     Route::resource('reelComments', ReelCommentsController::class);
     Route::resource('favorite', FavoriteController::class);

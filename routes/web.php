a031c1ac2a2d9d723efaf722e6e7b8a0089ebe08
Doha05a <mohamedlouayk@gmail.com>
2025-06-24 21:38:49 +0300

    تعديل واجهة لوحة التحكم20

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,11 @@ use Illuminate\Support\Facades\Mail;
 use App\Http\Controllers\AuthController;
 
 
-// Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth'])->group(function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
-// });
+});
 Route::post('/login_admin', [AuthController::class, 'login_admin'])->name('login_admin');
 Route::get('/login', function () {
     return view('sign-in');

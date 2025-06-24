11f06475fed81b7f75cd23d4cafc68ec6696c799
Doha05a <mohamedlouayk@gmail.com>
2025-06-24 23:18:57 +0300

    تعديل واجهة لوحة التحكم25

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -25,44 +25,70 @@
 
 <body>
 
-    <aside
-        class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4"
-        id="sidenav-main">
-        <div class="sidenav-header">
-            <a class="navbar-brand m-0" href="#">
-                <img src="{{ asset('assets/img/alpha3.png') }}" width="26px" height="26px" class="navbar-brand-img"
-                    alt="logo">
-                <span class="ms-1 font-weight-bold">لوحة التحكم إلفا</span>
-            </a>
-        </div>
-        <hr class="horizontal dark mt-0">
-        <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->is('dashboard') ? 'active bg-danger text-white' : '' }}"
-                        href="">
-                        <i class="ni ni-tv-2 text-sm opacity-10"></i>
-                        <span class="nav-link-text ms-1">الصفحة الرئيسية</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->is('users*') ? 'active bg-danger text-white' : '' }}"
-                        href="">
-                        <i class="ni ni-calendar-grid-58 text-sm opacity-10"></i>
-                        <span class="nav-link-text ms-1">إدارة المستخدمين</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->is('categories*') ? 'active bg-danger text-white' : '' }}"
-                        href="">
-                        <i class="ni ni-credit-card text-sm opacity-10"></i>
-                        <span class="nav-link-text ms-1">إدارة الأصناف</span>
-                    </a>
-                </li>
-                <!-- أضف باقي العناصر بنفس الطريقة -->
-            </ul>
-        </div>
-    </aside>
+  <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
+    <div class="sidenav-header">
+      <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+      <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
+        <img src="{{ asset('assets/img/alpha3.png') }}" width="26px" height="26px" class="navbar-brand-img h-100" alt="main_logo">
+        <span class="ms-1 font-weight-bold">لوحة التحكم  إلفا</span>
+      </a>
+    </div>
+    <hr class="horizontal dark mt-0">
+    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="../pages/dashboard.html">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-tv-2 text-dark text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">الصفحة الرئيسية</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="../pages/tables.html">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-calendar-grid-58 text-dark text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">إدارة المستخدمين</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="../pages/billing.html">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-credit-card text-dark text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">إدارة الأصناف</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="../pages/virtual-reality.html">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-app text-dark text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">إدارة المنتجات</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="../pages/rtl.html">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-world-2 text-dark text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">إدارة الإعلانات </span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="../pages/profile.html">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">إدارة الريلزات</span>
+          </a>
+        </li>
+
+      </ul>
+    </div>
+
+  </aside>
 
 
     {{-- المحتوى --}}

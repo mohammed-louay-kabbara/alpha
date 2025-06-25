fc09d6aca6be97608bfcf6b611896bb5c6d83b89
Doha05a <mohamedlouayk@gmail.com>
2025-06-25 20:44:43 +0300

    تعديل واجهة لوحة التحكم34

--- app/Http/Controllers/FollowerController.php
+++ app/Http/Controllers/FollowerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\follower;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FollowerController extends Controller
 {

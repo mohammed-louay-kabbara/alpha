3f99696f28037a74895f40f0538488ca07859274
Doha05a <mohamedlouayk@gmail.com>
2025-06-25 21:09:26 +0300

    تعديل واجهة لوحة التحكم37

--- app/Models/follower.php
+++ app/Models/follower.php
@@ -9,7 +9,7 @@ class follower extends Model
     protected $fillable = ['followed_id', 'follower_id' ];
     
     public function user(){
-    return $this->belongsTo(User::class);
+    return $this->belongsTo(User::class,'follower_id');
   }
 
 }

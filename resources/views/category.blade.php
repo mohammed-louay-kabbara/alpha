2d9925584f8696c63a594c958318ced461848869
Doha05a <mohamedlouayk@gmail.com>
2025-06-26 11:05:44 +0300

    تعديل واجهة لوحة التحكم47

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -128,11 +128,11 @@
                                 style="width: 40px; height: 40px; background-color: white">
 
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                                    fill="#000" style="color: #000; background-color: #000" class="bi bi-plus" viewBox="0 0 16 16">
                                     <path
                                         d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
                                 </svg>
-                                
+
                             </button>
                         </li>
                         <li class="nav-item d-xl-none ps-3 d-flex align-items-center">

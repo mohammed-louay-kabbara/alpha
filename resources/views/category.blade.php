c889e5092a2178002cd148ae120d3459364a1502
Doha05a <mohamedlouayk@gmail.com>
2025-06-26 11:00:55 +0300

    تعديل واجهة لوحة التحكم45

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -126,7 +126,7 @@
                         <li class="nav-item d-flex align-items-center">
                             <button
                                 class="btn btn rounded-circle d-flex align-items-center justify-content-center"
-                                style="width: 40px; height: 40px;">
+                                style="width: 40px; height: 40px; background-color: white">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                                     fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />

0588c4d6e9c8c50976bbf9317b46eec2b34a842b
Doha05a <mohamedlouayk@gmail.com>
2025-06-26 11:02:21 +0300

    تعديل واجهة لوحة التحكم46

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -124,15 +124,15 @@
                     </div>
                     <ul class="navbar-nav  justify-content-end">
                         <li class="nav-item d-flex align-items-center">
-                            <button
-                                class="btn btn rounded-circle d-flex align-items-center justify-content-center"
+                            <button class="btn btn rounded-circle d-flex align-items-center justify-content-center"
                                 style="width: 40px; height: 40px; background-color: white">
+
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                    fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                    fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
                                     <path
                                         d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
                                 </svg>
+                                
                             </button>
                         </li>
                         <li class="nav-item d-xl-none ps-3 d-flex align-items-center">

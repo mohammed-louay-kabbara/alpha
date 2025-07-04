e0653ee4d9967769445a9a00cf4a6ef3451c39a6
Doha05a <mohamedlouayk@gmail.com>
2025-07-05 00:47:38 +0300

    تعديل واجهة لوحة التحكم1645888549955544

--- resources/views/advertisements.blade.php
+++ resources/views/advertisements.blade.php
@@ -98,7 +98,7 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link active" href="{{ route('advertisement.index') }}">
-                        <div class="text-center">
+                        <div class="d-flex justify-content-center align-items-cente">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 class="bi bi-badge-ad" viewBox="0 0 16 16">
                                 <path
@@ -106,7 +106,7 @@
                                 <path
                                     d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2z" />
                             </svg>
-                                      <span class="nav-link-text ms-1">إدارة الإعلانات </span>
+                                  <span class="nav-link-text ms-1 ">إدارة الإعلانات </span>
                         </div>
               
                     </a>

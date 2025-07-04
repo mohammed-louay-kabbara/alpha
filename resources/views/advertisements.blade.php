7bd2f69d89bce6566ec4a82ac3aec23df8cc5e78
Doha05a <mohamedlouayk@gmail.com>
2025-07-05 00:50:27 +0300

    تعديل واجهة لوحة التحكم164588854995554877

--- resources/views/advertisements.blade.php
+++ resources/views/advertisements.blade.php
@@ -106,8 +106,9 @@
                                 <path
                                     d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2z" />
                             </svg>
-                                  <span class="nav-link-text ms-1 mr-28">إدارة الإعلانات </span>
+                                  
                         </div>
+                        <span class="nav-link-text ms-1 mr-28">إدارة الإعلانات </span>
               
                     </a>
                 </li>

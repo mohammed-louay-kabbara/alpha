d6f45e0f9b36eed44a6f24714b3a5a4e0e013c9d
Doha05a <mohamedlouayk@gmail.com>
2025-07-10 14:39:03 +0300

    Add json files to gitignore4

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -153,7 +153,7 @@
                     </div>
                     <ul class="navbar-nav  justify-content-end">
                         <li class="nav-item d-flex align-items-center">
-                            <a href="{{ route('EditProfileAdmin') }}" class="nav-link text-white font-weight-bold px-0">
+                            <a href="{{ route('edit') }}" class="nav-link text-white font-weight-bold px-0">
                                 <i class="fa fa-user me-sm-1"></i>
                                 <span class="d-sm-inline d-none">ملف السخصي</span>
                             </a>

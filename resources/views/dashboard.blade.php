3d41383d89fb82f3db6e7b796a7eedc6da77c570
Doha05a <mohamedlouayk@gmail.com>
2025-07-12 12:21:58 +0300

    Add json files to 377

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -471,9 +471,11 @@
                                                 <i class="ni ni-mobile-button text-white opacity-10"></i>
                                             </div>
                                             <div class="d-flex flex-column">
-                                                <h6 class="mb-1 text-dark text-sm">{{ $item->name }}</h6>
                                                 <h6 class="mb-1 text-dark text-sm">{{ $item->followers_count }}</h6>
                                                 <span class="text-xs">{{ $item->email }}</span>
+                                            </div>
+                                            <div class="d-flex flex-column">
+                                                <h6 class="mb-1 text-dark text-sm">{{ $item->followers_count }}</h6>
                                             </div>
                                         </div>
                                     </li>

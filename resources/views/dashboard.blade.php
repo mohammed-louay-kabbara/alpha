0b402df0e33cbd5f349ffab80220ea9aa4e1dbd8
Doha05a <mohamedlouayk@gmail.com>
2025-07-16 18:27:52 +0300

    Add json files to 604

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -381,15 +381,11 @@
                 <div class="col-lg-7 mb-lg-0 mb-4">
                     <div class="card z-index-2 h-100">
                         <div class="card-header pb-0 pt-3 bg-transparent">
-                            <h6 class="text-capitalize">Sales overview</h6>
-                            <p class="text-sm mb-0">
-                                <i class="fa fa-arrow-up text-success"></i>
-                                <span class="font-weight-bold">4% more</span> in 2021
-                            </p>
+                            <h6 class="text-capitalize">عدد المستخدمين الجدد</h6>
                         </div>
                         <div class="card-body p-3">
                             <div class="chart">
-                                <canvas id="userChart" class="chart-canvas" height="300"></canvas>
+                                <canvas id="userChart" class="chart-canvas"></canvas>
                             </div>
                         </div>
                     </div>

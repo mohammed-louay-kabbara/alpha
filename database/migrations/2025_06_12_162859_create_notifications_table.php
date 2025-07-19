633d2466b6542254851f7a18e7ba76b95fd5261c
Doha05a <mohamedlouayk@gmail.com>
2025-07-19 16:20:47 +0300

    Add json files to 100

--- database/migrations/2025_06_12_162859_create_notifications_table.php
+++ database/migrations/2025_06_12_162859_create_notifications_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('type');
-            $table->morphs('notifiable');
-            $table->text('data');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('body');
             $table->timestamp('read_at')->nullable();
             $table->timestamps();
         });

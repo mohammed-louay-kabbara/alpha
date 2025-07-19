6481cd3a2792693b0495d231e1ffb2a87eaec0be
Doha05a <mohamedlouayk@gmail.com>
2025-07-19 10:37:12 +0300

    Add json files to 90

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -80,7 +80,7 @@ class AuthController extends Controller
     public function pictureupdate(Request $request)
     {
         $user = Auth::user();
-        dd($request);
+        dd($request->picture);
         $imagePath = $request->file('picture')->store('profile_pictures', 'public');
         if ($user->picture=="profile_pictures/defoult_image.jpg") {
             $user->update(['picture' => $imagePath ]);

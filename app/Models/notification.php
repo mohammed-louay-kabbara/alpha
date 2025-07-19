13bc9f2101589eaed287893f6247384fcc5dcc85
Doha05a <mohamedlouayk@gmail.com>
2025-07-19 16:44:11 +0300

    Add json files to 103

---
+++ app/Models/notification.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class notification extends Model
+{
+    protected $fillable = [
+        'user_id', 'title', 'body', 'read_at', 'sender_id'
+    ];
+
+    public function user(){
+      return $this->belongsTo(User::class);
+    }
+
+}

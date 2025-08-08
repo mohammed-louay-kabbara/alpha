3c8f177e7cd23b73ec4101ec9a9c4fcb84788740
Doha05a <mohamedlouayk@gmail.com>
2025-08-08 18:39:30 +0300

    Add json files to 1

--- app/Models/reels.php
+++ app/Models/reels.php
@@ -6,9 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class reels extends Model
 {
-    protected $fillable = [
-      'user_id' , 'media_path' , 'description' , 'likes_count' , 'dislikes_count' ,'thumbnail_path'
-    ];
+    protected $fillable = [ 'user_id' , 'media_path' , 'description' , 'likes_count' , 'dislikes_count' ,'thumbnail_path'];
 
     public function user()
     {

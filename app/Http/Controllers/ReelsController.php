cc764187528ba1165517de33fec7ff4ed1248fed
Doha05a <mohamedlouayk@gmail.com>
2025-08-09 20:26:29 +0300

    Add json files to 1

--- app/Http/Controllers/ReelsController.php
+++ app/Http/Controllers/ReelsController.php
@@ -15,27 +15,6 @@ class ReelsController extends Controller
     public function index()
     {
         $userId = auth()->id();
-        $reels = Reels::with('user')
-            ->orderBy('created_at', 'desc')
-            ->get()
-            ->map(function ($reel) use ($userId) {
-                $reel->is_following = \App\Models\Follower::where('follower_id', $userId)
-                    ->where('followed_id', $reel->user_id)
-                    ->exists();
-                return $reel;
-            })
-            ->map(function ($reel) {
-            $reel->liked_by_user = $reel->likes->contains('user_id', auth()->id());
-            unset($reel->likes);
-            return $reel;
-            });
-        return response()->json($reels);
-    }
-
-    public function getReels()
-    {
-        $userId = auth()->id();
-
         // 1️⃣ الأشخاص الذين أتابعهم
         $followingIds = \App\Models\Follower::where('follower_id', $userId)
             ->pluck('followed_id');
@@ -69,6 +48,28 @@ class ReelsController extends Controller
             });
 
         return response()->json($reels);
+
+        // $userId = auth()->id();
+        // $reels = Reels::with('user')
+        //     ->orderBy('created_at', 'desc')
+        //     ->get()
+        //     ->map(function ($reel) use ($userId) {
+        //         $reel->is_following = \App\Models\Follower::where('follower_id', $userId)
+        //             ->where('followed_id', $reel->user_id)
+        //             ->exists();
+        //         return $reel;
+        //     })
+        //     ->map(function ($reel) {
+        //     $reel->liked_by_user = $reel->likes->contains('user_id', auth()->id());
+        //     unset($reel->likes);
+        //     return $reel;
+        //     });
+        // return response()->json($reels);
+    }
+
+    public function getReels()
+    {
+
     }
     
     public function create()
